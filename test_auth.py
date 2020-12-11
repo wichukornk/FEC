e35b955a3ba7229e1ba0904555c0c34a681b68f0
wichu_win10 <wichukorn.ku@outlook.com>
2020-12-11 17:12:05 +0700

    Update test_auth.py

--- test_auth.py
+++ test_auth.py
@@ -1,21 +1,42 @@
-import pandas as pd
+import mysql.connector
 import os
-from google.oauth2 import service_account
-thisdir="C:/Users/wichu/Documents"
-path_list=[]
-for r, d, f in os.walk(thisdir):
-    for file in f:
-        if file.endswith(".csv"):
-            #print(os.path.join(r, file))
-            path_list.append(os.path.join(r, file))
-df=pd.DataFrame(path_list,columns=['Path'])
-#print(df)
 
-credentials = service_account.Credentials.from_service_account_file(
-   "C:/Users/wichu/Documents/GitHub/FEC/IntroCloud01-7b6a00779302.json",
+def list_file(pth,files):
+    thisdir=pth
+    path_list=[]
+    for r, d, f in os.walk(thisdir):
+        for file in f:
+            if file.endswith(files):
+                path_list.append(os.path.join(r, file))
+    return path_list
+
+def create_pthTurple(path_list):
+    path_turple=[]
+    for i in range(len(path_list)):
+        t=(str(i),path_list[i])
+        path_turple.append(t)
+    return path_turple
+
+path_list=list_file("C:/Users/wichu/Documents/GitHub",".csv")
+val=create_pthTurple(path_list)
+
+#print(val)
+
+mydb = mysql.connector.connect(
+  host="163.44.196.192",
+  user="ararize",
+  password="isylzjko$3rv3r",
+  database="tp"
 )
 
-#pandas_gbq.context.credentials = credentials
+#print(mydb)
 
+mycursor = mydb.cursor()
 
-df.to_gbq(destination_table='FEC.pathList_sim',project_id='introcloud01',if_exists='replace',credentials=credentials)
+mycursor.execute("TRUNCATE TABLE vm_path")
+
+sql="INSERT INTO vm_path (pthID, pthName) VALUES (%s, %s)"
+
+mycursor.executemany(sql,val)
+mydb.commit()
+print(mycursor.rowcount)
